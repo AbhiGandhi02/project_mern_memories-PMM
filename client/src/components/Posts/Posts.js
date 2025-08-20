ac11add6cab8a6654e2d38e9be66901232584b2a
AbhiGandhi02 <abhi.24bcs10397@sst.scaler.com>
2025-08-20 23:07:05 +0530

    Clear button is now working for the choose file input.

--- client/src/components/Posts/Posts.js
+++ client/src/components/Posts/Posts.js
@@ -1,24 +1,48 @@
 import React from 'react';
-import { Grid, CircularProgress } from '@material-ui/core';
+import { Grid, CircularProgress, Typography, Paper } from '@material-ui/core';
 import { useSelector } from 'react-redux';
 
 import Post from './Post/Post';
 import useStyles from './styles';
 
+// Changed this file removed loader and created a div which shows no post made.
 const Posts = ({ setCurrentId }) => {
   const posts = useSelector((state) => state.posts);
   const classes = useStyles();
 
+  const isLoading = posts === null || posts === undefined;
+
+  const hasNoPosts = !isLoading && posts.length === 0;
+
+  if (isLoading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', marginTop: '2rem' }}>
+        <CircularProgress />
+      </div>
+    );
+  }
+
+  if (hasNoPosts) {
+    return (
+      <Paper style={{ padding: '2rem', textAlign: 'center', marginTop: '2rem' }}>
+        <Typography variant="h6" color="textSecondary">
+          No posts made yet
+        </Typography>
+        <Typography variant="body2" color="textSecondary" style={{ marginTop: '1rem' }}>
+          Be the first to create a memory!
+        </Typography>
+      </Paper>
+    );
+  }
+
   return (
-    !posts.length ? <CircularProgress /> : (
-      <Grid className={classes.container} container alignItems="stretch" spacing={3}>
-        {posts.map((post) => (
-          <Grid key={post._id} item xs={12} sm={6} md={6}>
-            <Post post={post} setCurrentId={setCurrentId} />
-          </Grid>
-        ))}
-      </Grid>
-    )
+    <Grid className={classes.container} container alignItems="stretch" spacing={3}>
+      {posts.map((post) => (
+        <Grid key={post._id} item xs={12} sm={6} md={6}>
+          <Post post={post} setCurrentId={setCurrentId} />
+        </Grid>
+      ))}
+    </Grid>
   );
 };
 
--- server/index.js
+++ server/index.js
@@ -14,11 +14,9 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://js_mastery:123123123@practice.jto9p.mongodb.net/test';
+const CONNECTION_URL = 'mongodb://localhost:27017/memories_test';
 const PORT = process.env.PORT|| 5000;
 
-mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(CONNECTION_URL, { serverSelectionTimeoutMS: 20000 })
   .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
   .catch((error) => console.log(`${error} did not connect`));
-
-mongoose.set('useFindAndModify', false);
